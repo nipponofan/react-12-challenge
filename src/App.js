f0f0a6a75f847cbab60d7724dbac86fb9815b470
Barbara Paul <barbara.kikou@gmail.com>
2019-11-13 19:10:48 +0100

    exercise done

--- src/App.js
+++ src/App.js
@@ -5,29 +5,39 @@ import './App.css';
 const Home = () => <p>This is the Home component!</p>;
 const About = () => <p>This challenge is about tabs!</p>;
 const Contact = () => <p>Feel free to contact us!</p>;
-const Error = () => <p>Something went <strong>wrong</strong>!</p>;
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeId: 'home'
+      activeId: ''
     };
+    this.handleChangeTab = this.handleChangeTab.bind(this);
+  }
+
+
+  handleChangeTab(event) {
+    const buttonId = event.target.id;
+    this.setState({ activeId: buttonId });
   }
 
   getTabContent() {
     switch(this.state.activeId) {
       case 'home':
         return <Home />;
-      default:
-        return <Error />;
+      case 'contact':
+        return <Contact />;
+      case 'about':
+        return <About />;
+
     }
   }
 
   render() {
     return (
       <div className="App">
-        <TabSelector />
+        <TabSelector activeId={this.state.activeId} handleChangeTab={this.handleChangeTab} />
         <div className="App-content">
           {
             this.getTabContent()
